5865aded978ba2fc25f4ceaf295b7bd04a5db37f
cinicuts0-boop <cinicuts0@gmail.com>
2026-03-30 17:33:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+
 import requests
-import pandas as pd
 import time
-import ta
 import os
 
 TELEGRAM_TOKEN = os.getenv("8562765008:AAG4-qmd9949TGGQ7F5nGkOMMhXBdZlm8Ng")
@@ -9,79 +8,11 @@ CHAT_ID = os.getenv("8007854479")
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+    res = requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+    print(res.text)
 
-def get_data():
-    url = "https://api.binance.com/api/v3/klines?symbol=ETHUSDT&interval=5m&limit=100"
-
-    try:
-        res = requests.get(url)
-        data = res.json()
-
-        df = pd.DataFrame(data, columns=[
-            "time","open","high","low","close","volume",
-            "ct","qav","nt","tbv","tqv","ignore"
-        ])
-
-        df['close'] = df['close'].astype(float)
-        df['high'] = df['high'].astype(float)
-        df['low'] = df['low'].astype(float)
-
-        return df
-
-    except Exception as e:
-        print("Data Error:", e)
-        return None
-
-def strategy():
-    df = get_data()
-
-    if df is None or df.empty:
-        return None
-
-    # Keltner Channel
-    kc = ta.volatility.KeltnerChannel(
-        high=df['high'],
-        low=df['low'],
-        close=df['close'],
-        window=20
-    )
-
-    df['upper'] = kc.keltner_channel_hband()
-    df['lower'] = kc.keltner_channel_lband()
-
-    # RSI
-    df['rsi'] = ta.momentum.RSIIndicator(df['close'], window=14).rsi()
-
-    last = df.iloc[-1]
-
-    print("Price:", last['close'], "RSI:", last['rsi'])
-
-    # BUY
-    if last['close'] < last['lower'] and last['rsi'] < 50:
-        return f"📈 BUY ETH\nPrice: {last['close']}"
-
-    # SELL
-    elif last['close'] > last['upper'] and last['rsi'] > 50:
-        return f"📉 SELL ETH\nPrice: {last['close']}"
-
-    return None
-
-last_signal = None
-
-print("Keltner Bot Started...")
+print("Test Bot Started...")
 
 while True:
-    try:
-        signal = strategy()
-
-        if signal and signal != last_signal:
-            send_telegram(f"🚨 ETH Keltner Signal:\n{signal}")
-            last_signal = signal
-        else:
-            print("No signal")
-
-    except Exception as e:
-        print("Error:", e)
-
-    time.sleep(300)
+    send_telegram("✅ TEST MESSAGE ETH BOT")
+    time.sleep(60)
