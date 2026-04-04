036b82e14c8aa836062d42b61017bd5fac75f6ed
cinicuts0-boop <cinicuts0@gmail.com>
2026-04-04 09:19:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import threading
+import time
+
+from web.routes import app
+from bot.signals import get_signal_for
+
+def run_bot():
+    while True:
+        get_signal_for("ETH-USD", "ETH")
+        get_signal_for("BTC-USD", "BTC")
+        get_signal_for("^NSEI", "NIFTY")
+        get_signal_for("^NSEBANK", "BANKNIFTY")
+        get_signal_for("CL=F", "CRUDE")
+
+        time.sleep(300)
+
+if __name__ == "__main__":
+    threading.Thread(target=run_bot, daemon=True).start()
+    app.run(host="0.0.0.0", port=8080)
