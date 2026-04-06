6748ba72ec5ecf99e551df3cef7b7e1a1ed4efdb
cinicuts0-boop <cinicuts0@gmail.com>
2026-04-06 13:37:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -383,7 +383,7 @@ def coin_detail(name):
     <html>
     <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="60">
+    <meta http-equiv="refresh" content="360">
 
     <style>
     body {{
